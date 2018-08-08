4348d266e2dc33c30d1ce716ddb5c376ce8f2d70
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-08 02:26:04 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,17 +1,17 @@
 def division(num1, num2)
- 
+  return num1/num2
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(phrase)
+  return phrase
 end
 
-def greeting
-
+def greeting(greeting, name)
+  return greeting + " " + name
 end
 
 def return_a_value
